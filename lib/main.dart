005d1bd4611627f7fd492a0514ef9850620415f1
osa989 <osamagamal9988@gmail.com>
2024-09-28 20:53:43 +0300

    initialize the app

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MediaQueryTest());
+}
+
+class MediaQueryTest extends StatelessWidget {
+  const MediaQueryTest({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
